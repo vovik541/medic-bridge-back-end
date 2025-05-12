03a5c817e5aee95b44d10b561ca97c3840f04c2d
vovik541 <conanzhill@gmail.com>
2025-05-12 07:24:40 -0700

    springdoc-openapi-starter-webmvc-ui v2.1+ swagger UI url returns 404 error, with security 403, additional configuration didn't help to find the reason. @ControllerAdvice with openapi v2.1- kills swagger UI, disabled @ControllerAdvice. To be investigated later

--- src/main/java/com/bridge/medic/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/bridge/medic/exception/handler/GlobalExceptionHandler.java
@@ -1,18 +1,18 @@
-package com.bridge.medic.exception.handler;
-
-import com.bridge.medic.exception.dto.ErrorResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ControllerAdvice
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(value = Exception.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public @ResponseBody ErrorResponse handleException(Exception ex) {
-        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
-    }
-}
+//package com.bridge.medic.exception.handler;
+//
+//import com.bridge.medic.exception.dto.ErrorResponse;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+//
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//
+//    @ExceptionHandler(value = Exception.class)
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    public @ResponseBody ErrorResponse handleException(Exception ex) {
+//        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
+//    }
+//}
