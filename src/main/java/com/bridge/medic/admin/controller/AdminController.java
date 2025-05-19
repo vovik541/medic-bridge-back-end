b9d7fa3f7b2888f22a256bbfa4e7047d559ac5b8
vovik541 <conanzhill@gmail.com>
2025-05-19 07:27:28 +0300

    admin change user role endpoint added

--- src/main/java/com/bridge/medic/admin/controller/AdminController.java
+++ src/main/java/com/bridge/medic/admin/controller/AdminController.java
@@ -1,13 +1,25 @@
 package com.bridge.medic.admin.controller;
 
+import com.bridge.medic.config.security.authorization.RoleEnum;
+import com.bridge.medic.config.security.service.AuthenticatedUserService;
+import com.bridge.medic.user.model.User;
+import com.bridge.medic.user.service.UserService;
 import io.swagger.v3.oas.annotations.Hidden;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/v1/admin")
 @PreAuthorize("hasRole('ADMIN')")
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final UserService userService;
+    private final AuthenticatedUserService authenticatedUserService;
 
     @GetMapping
     @PreAuthorize("hasAuthority('admin:read')")
@@ -20,6 +32,16 @@ public class AdminController {
     @Hidden
     public String post() {
         return "POST:: admin controller";
+    }
+    @PostMapping("/change-role")
+    public ResponseEntity<?> changeUserRole(@RequestParam("userId") Long userId,
+                                                @RequestParam("newRole") String newRole) {
+        if (Stream.of(RoleEnum.values()).map(Enum::name).anyMatch(x -> x.equals(newRole))){
+            userService.changeUserRole(userId, RoleEnum.valueOf(newRole));
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.badRequest().build();
     }
 
     @PutMapping
